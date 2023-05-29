f2eff7a72acbc2ceb4ca15332f84eed075605665
simaodiazz <105567918+simaodiazz@users.noreply.github.com>
2023-05-29 18:30:12 +0100

    Updated!

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+// A função 
+app.use(express.json());
+
--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,3 +1,7 @@
 const clickButton = document.getElementById("click");
 
 clickButton.addEventListener('click', handleButtonClick);
+
+function handleButtonClick() {
+    // Aqui vai ser adicionada a pontuação
+}
